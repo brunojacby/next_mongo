e46d20e3a23ab38c7342c1629613b02e5c79bb8b
Bruno Jacby <bruno.jacby@dexworks.com>
2024-08-17 20:24:03 +0100

    NXM - Add DeleteBlock component

---
+++ ticket-app/app/(components)/DeleteBlock.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const DeleteBlock = ({ id }) => {
+  const router = useRouter();
+
+  const deleteTicket = async () => {
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+      method: "DELETE",
+    });
+    if (res.ok) {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <FontAwesomeIcon
+        icon={faX}
+        className="text-red-400 hover:cursor-pointer hover:text-red-200"
+        onClick={deleteTicket}
+      />
+    </div>
+  );
+};
+
+export default DeleteBlock;
