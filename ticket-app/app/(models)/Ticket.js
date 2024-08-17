c142f2cd9104c22616f83ca8f03557a152b1ec8a
Bruno Jacby <bruno.jacby@dexworks.com>
2024-08-17 20:25:55 +0100

    NXM - Add routing and api

---
+++ ticket-app/app/(models)/Ticket.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.Promise = global.Promise;
+
+const ticketSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    category: String,
+    priority: Number,
+    progress: Number,
+    status: String,
+    active: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Ticket = mongoose.models.TicketDB || mongoose.model("TicketDB", ticketSchema);
+export default Ticket;
